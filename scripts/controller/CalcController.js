a15ae10c0df6870e1ad91a2f6af7a4304375bb90
Francisco Sousa <franciscoeds1987@gmail.com>
2020-05-29 08:32:57 -0300

    Manipulando o DOM

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -3,6 +3,17 @@ class CalcController {
     constructor() {
         this._displayCalc = "0"
         this._currentDate
+        this.initialize()
+    }
+
+    initialize() {
+        let displayCalcEl = document.querySelector("#display")
+        let dateEl = document.querySelector("#data")
+        let timeEl = document.querySelector("#hora")
+
+        displayCalcEl.innerHTML = "4567"
+        dateEl.innerHTML = "01/05/2020"
+        timeEl.innerHTML = "00:00"
     }
 
     get displayCalc() {
