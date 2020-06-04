a20a2364ffe7833d96843b558403c748d3c3843e
Francisco Sousa <franciscoeds1987@gmail.com>
2020-06-04 04:30:03 -0300

    Ctrl + c e Ctrl + v

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -14,6 +14,23 @@ class CalcController {
         this.initKeyboard()
     }
 
+    pasteFromClipboard() {
+        document.addEventListener('paste', e => {
+           let text = e.clipboardData.getData('Text')
+            this.displayCalc = parseFloat(text)
+            console.log(text)
+        });
+    }
+
+    copyToClipboard() {
+        let input = document.createElement('input')
+        input.value = this.displayCalc
+        document.body.appendChild(input)
+        input.select()
+        document.execCommand('Copy')
+        input.remove()
+    }
+
     initialize() {
         this.setDisplayDateTime()
 
@@ -21,6 +38,7 @@ class CalcController {
             this.setDisplayDateTime()
         }, 1000);
         this.setLastNumberToDisplay()
+        this.pasteFromClipboard()
     }
 
     initKeyboard() {
@@ -58,6 +76,9 @@ class CalcController {
                 case '8':
                 case '9':
                     this.addOperation(parseInt(e.key))
+                    break
+                case 'c':
+                    if (e.ctrlKey) this.copyToClipboard()
                     break
             }
         });
